39e5b2d665e85b12e706211fba86c616c7f6a7b7
THAM <thampk@outlook.com>
2022-04-29 23:05:50 +0700

    [START]build totorial php phpmySQL

---
+++ dump/myDb.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS myDb;
+USE myDb;
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+  time_zone = "+01:00";
+CREATE TABLE `Person` (
+    `id` int(11) NOT NULL,
+    `name` varchar(20) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+INSERT INTO
+  `Person` (`id`, `name`)
+VALUES
+  (1, 'Dang'),
+  (2, 'Sasada'),
+  (3, 'Tony');
